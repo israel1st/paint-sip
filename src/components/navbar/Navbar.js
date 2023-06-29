71c44c68bba0bbb2f0c325cb0680889923bf4007
israel1st <israelbassey.ib.com>
2023-06-29 14:07:38 -0400

    commit message for the changes could be:
    "Added/modified navbar component and associated files:
    
    Modified src/App.js
    Added src/components/navbar/Navbar.css
    Added src/components/navbar/Navbar.js
    Added src/components/navbar/img/icon1.png
    Added src/components/navbar/img/icon2.png
    Added src/components/navbar/img/paint.jpeg

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import icon1 from './img/icon1.png';
+import icon2 from './img/icon2.png';
+import './Navbar.css';
+
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <div>
+            <nav className="navbar">
+                <div className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                    <div className="hamburger"></div>
+                </div>
+
+                <ul className={`menu ${menuOpen ? 'open' : ''}`}>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">SERVICES</a></li>
+                    <li><a href="#">GALLERY</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li className="get-quote"><a href="#">GET A QUOTE</a></li>               
+                </ul>
+            </nav>
+            <img src={icon1} alt="Icon 1" className="small-image" />
+            <img src={icon2} alt="Icon 1" className="small-image2" />
+        </div>
+    );
+};
+
+export default Navbar;
+
